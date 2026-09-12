7916d26b1e32a5b6ef4cd480c499598b2aad9c77
Karl <smitzkar@gmail.com>
2026-09-12 20:05:27 +0200

    Fix icon?

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // IMPORTANT: changes in CACHE_NAME trigger local updates (doesn't manually check if file content changed)
 //            -> if changing anything on server, don't forget to update the version to force cache updates
 
-const CACHE_NAME = "seal-counter-v0.2.2";
+const CACHE_NAME = "seal-counter-v0.2.3";
 
 const CACHED_URLS = [
   "./",
@@ -23,7 +23,8 @@ const CACHED_URLS = [
   "./js/strings.js",
   "./js/welcome.js",
   "./js/app.js",
-  "./assets/visual-guide-placeholder.svg"
+  "./assets/visual-guide-placeholder.svg",
+  "./assets/favicon.ico"
 ];
 
 // self.addEventListener("install", (event) => {
